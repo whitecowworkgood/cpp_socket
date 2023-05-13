1f34d1b8810673e608f4d801a74c0e471ab57e9e
whitecowworkgood <dlawowns382@naver.com>
2023-05-13 11:23:14 +0900

    add socket

--- client.cpp
+++ client.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 #include<string>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include<cstdio>
 
 int main(int argc, char* argv[]) {
     std::string ServerIp, ServerPort, File, ServerPath;
@@ -34,11 +40,39 @@ int main(int argc, char* argv[]) {
             
     }
     if(ServerIp.empty() || ServerPort.empty() || File.empty()|| ServerPath.empty()) {
-            std::cout << "올바른 옵션이 제공되지 않았습니다." << std::endl;
+            std::cout << "Options Failed." << std::endl;
             return 1;
     }
+    const char* ServerIpStr = ServerIp.c_str();
+    const char* ServerPortStr = ServerPort.c_str();
+    const char* FileStr = File.c_str();
+    const char* ServerPathStr = ServerPath.c_str();
+    int sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1){
 
+    }
 
+    
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr(ServerIpStr);
+    serv_addr.sin_port=htons(atoi(ServerPortStr));
+
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) {
+
+    }
+
+    char message[30];
+    int str_len;
+    str_len=read(sock, message, sizeof(message)-1);
+    if(str_len==-1){
+
+    }
+    write(sock, ServerPathStr, sizeof(ServerPathStr));
+	
+    printf("Message from server: %s \n", message);  
+    close(sock);
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include<string>
-
-
+#include <sys/socket.h>  // 소켓 관련 함수와 데이터 타입
+#include <netinet/in.h>  // 인터넷 주소 구조체와 상수
+#include <arpa/inet.h>   // IP 주소 변환 함수
+#include <unistd.h>      // 파일 디스크립터 관련 함수
+#include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char* argv[]) {
     std::string Port;
@@ -17,11 +21,58 @@ int main(int argc, char* argv[]) {
     }
 
     if(Port.empty()) {
-        std::cout << "올바른 옵션이 제공되지 않았습니다." << std::endl;
+        std::cout << "Options Failed" << std::endl;
         return 1;
     }
+    // 소켓 생성
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cout << "Failed Socket Create" << std::endl;
+        return 1;
+    }
+    const char* portStr = Port.c_str();
 
-    
+    int socketfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        // 소켓 생성 실패
+        // 오류 처리
+    }
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    // INADDR_ANY는 서버 자신의 IP주소
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    const char* PortStr = Port.c_str();
+    serv_addr.sin_port=htons(atoi(PortStr));
+
+     if(bind(socketfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 ){
+
+     }
+        //error_handling("bind() error"); 
+    if(listen(socketfd, 5)==-1){
+
+    }
+        //error_handling("listen() error");
+
+
+    struct sockaddr_in clientAddress;
+
+    socklen_t clnt_addr_size=sizeof(clientAddress);
+    int clnt_sock=accept(socketfd, (struct sockaddr*)&socketfd,&clnt_addr_size);
+    if(clnt_sock==-1){
+
+    }
+        //error_handling("accept() error"); 
+
+    char message[]="Hello World!";
+    write(clnt_sock, message, sizeof(message));
+    // 서버, 클라이언트 소켓의 연결을 해제
+
+    read(clnt_sock, message, sizeof(message)-1);
+
+    std::cout<<message<<std::endl;
+
+    close(clnt_sock);	
     
     return 0;
 }
